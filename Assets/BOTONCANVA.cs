eedbd9ac9af693d40be3fc8949ce7de6863bde69
AlejandRangel1 <tic-300034@utnay.edu.mx>
2024-07-20 11:59:14 -0700

    project start

---
+++ Assets/BOTONCANVA.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOTONCANVA : MonoBehaviour
+{
+    public void Menu()
+    {
+        Debug.Log("Ir a menu");
+    }
+
+}
