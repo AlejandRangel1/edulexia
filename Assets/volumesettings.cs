9840cd132460bead6d3b904a2ea964d7fc9a899a
AlejandRangel1 <tic-300034@utnay.edu.mx>
2024-07-23 19:34:25 -0700

    Volunen funcional

---
+++ Assets/volumesettings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class volumesettings : MonoBehaviour
+{
+
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+
+}
