c0673af7948a34b1c8bec71c5040b190f04fa77d
MarianaL19 <marianajlopez19@gmail.com>
2024-07-31 10:34:50 -0600

    upd: ajustes para el build

--- Assets/Scripts/MainMenu/MenuController.cs
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
+    public Button btnJuegoLetraSonido;
     public GameObject popUp;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Asignar el método PasarANivel al evento onClick del botón
+        btnJuegoLetraSonido.onClick.AddListener(() => PasarANivel("MinijuegoLetraSonido"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void MostrarPopUp()
@@ -24,5 +30,10 @@ public class MenuController : MonoBehaviour
     public void CerrarPopUp()
     {
         popUp.SetActive(false);
+    }
+
+    public void PasarANivel(string nombreEscena)
+    {
+        SceneManager.LoadScene(nombreEscena);
     }
 }
